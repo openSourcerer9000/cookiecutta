055f355e1e0d97da5d8e12fac14ecb8fe36762cc
openSourcerer <seanrm100@gmail.com>
2023-01-25 09:57:44 -0600

    conda

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,8 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
-
+def remCondaFile(filepath):
+    os.remove(os.path.join(PROJECT_DIRECTORY,'conda-recipe', filepath))
 
 if __name__ == '__main__':
 
@@ -14,9 +15,9 @@ if __name__ == '__main__':
         remove_file('AUTHORS.rst')
         remove_file('docs/authors.rst')
 
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
+    if '{{ cookiecutter.use_conda }}' == 'n':
+        [remCondaFile(f) for f in ('bld.bat','build.sh','conda_build_config.yaml','meta.yaml')]
+        os.rmdir(os.path.join(PROJECT_DIRECTORY,'conda-recipe'))
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
